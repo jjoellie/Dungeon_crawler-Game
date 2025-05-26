dde8050f03ce88effaa9e9abd3a3383cfb17c9fd
Joel Junior Leke Akem <juniorlekeakem@gmail.com>
2025-05-26 21:36:10 +0200

    Update bitwise gevecht

--- Dungeon_crawler.c
+++ Dungeon_crawler.c
@@ -1,67 +1,37 @@
-typedef struct Room {
-    int id;
-    int num_neighbors;
-    struct Room** neighbors; // adjacency list
-} Room;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Allocatie op heap
-Room* create_room(int id) {
-    Room* room = malloc(sizeof(Room));
-    room->id = id;
-    room->num_neighbors = 0;
-    room->neighbors = malloc(sizeof(Room*) * 4); // max 4 buren
-    return room;
-}
+typedef struct {
+    int hp;
+    int damage;
+} Player;
 
-void connect_rooms(Room* a, Room* b) {
-    if (a->num_neighbors < 4 && b->num_neighbors < 4) {
-        a->neighbors[a->num_neighbors++] = b;
-        b->neighbors[b->num_neighbors++] = a;
-    }
-}
-
-typedef struct Monster {
+typedef struct {
     int hp;
     int damage;
 } Monster;
 
-typedef struct Room {
-    int id;
-    int numConnections;
-    struct Room** connections;
-    Monster* monster;  // NULL als er geen monster is
-} Room;
-
-typedef struct Player {
-    int currentRoomId;
-    int hp;
-    int damage;
-} Player;
-void fight(Player* player, Room* room) {
-    if (room->monster == NULL) {
-        printf("Er is geen monster in deze kamer.\n");
-        return;
-    }
-
-    printf("Gevecht gestart!\n");
-
-    // Speler en monster vallen elkaar aan
-    room->monster->hp -= player->damage;
-    player->hp -= room->monster->damage;
-
-    printf("Speler HP: %d\n", player->hp);
-    printf("Monster HP: %d\n", room->monster->hp);
-
-    // Check of iemand dood is
-    if (room->monster->hp <= 0) {
-        printf("Monster is verslagen!\n");
-        free(room->monster);
-        room->monster = NULL;
+void fight(Player *player, Monster *monster) {
+    while (player->hp > 0 && monster->hp > 0) {
+        int random = rand() % 17; // 0–16
+        for (int i = 0; i < 5 && player->hp > 0 && monster->hp > 0; i++) {
+            int bit = (random >> i) & 1;
+            if (bit == 0) {
+                // Monster valt speler aan
+                player->hp -= monster->damage;
+                printf("Monster valt aan! Speler HP: %d\n", player->hp);
+            } else {
+                // Speler valt monster aan
+                monster->hp -= player->damage;
+                printf("Speler valt aan! Monster HP: %d\n", monster->hp);
+            }
+        }
     }
 
     if (player->hp <= 0) {
-        printf("Speler is verslagen!\n");
-        // Hier zou je kunnen afsluiten of andere game over logica doen
+        printf("Speler is verslagen.\n");
+    } else if (monster->hp <= 0) {
+        printf("Monster is verslagen.\n");
     }
 }
-
